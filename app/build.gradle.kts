0339e2ed8964ed638e7c2c8d34429c7c66be6a33
Kufik <127012125+Onmaynec@users.noreply.github.com>
2026-08-03 16:43:03 +0300

    chore(app): bump version to 0.3.0-alpha.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.connectx"
         minSdk = 29
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.3.0-alpha.1"
+        versionCode = 9
+        versionName = "0.3.0-alpha.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
